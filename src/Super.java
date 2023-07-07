629b61dfcaca1328cc3852f0804eb6b714ee58f3
Karan <you@example.com>
2023-07-07 11:57:00 +0530

    Second Commit

---
+++ src/Super.java
@@ -0,0 +1,20 @@
+public class Super {
+
+    public void base(){
+        System.out.println("Hello Karan");
+    }
+}
+class dd extends Super{
+    public void derived1(){
+        super.base();
+        System.out.println("Hello Vish");
+    }
+
+}
+class inheritance1 {
+    public static void main(String[] args) {
+        dd obj1 = new dd();
+        obj1.derived1();
+    }
+}
+
